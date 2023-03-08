d57d59508663f8c7c137db290921c7bade054fbe
Flavio Daniel <flaviodanielgs@gmail.com>
2023-03-08 17:18:11 -0300

    Adicionar Formik para criar formularios e YUP para validar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,98 +1,121 @@
-import { useEffect, useState } from "react";
+import * as Yup from "yup";
 
 import ReactGoogleAutocomplete from "react-google-autocomplete";
 import axios from "axios";
+import { useFormik } from "formik";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function Home() {
-  const [cidadeOrigem, setCidadeOrigem] = useState("");
-  const [cidadeDestino, setCidadeDestino] = useState("");
-  const [tipoCarro, setTipoCarro] = useState("Básico");
-  const [valorAluguel, setValorAluguel] = useState(0);
   const [erro, setErro] = useState(false);
   const router = useRouter();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setErro(false);
+  //Tipo de carros disponíveis
+  const tipoCarroOptions = [
+    { value: "Básico" },
+    { value: "Intermediário" },
+    { value: "Luxo" },
+  ];
 
-    try {
-      const response = await axios.get(
-        `/api/maps/api/distancematrix/json?origins=${cidadeOrigem}&destinations=${cidadeDestino}&units=imperial&key=AIzaSyAQlXH_lfQPVBxaO5SEifHsSVQou9d_etY`
-      );
+  //Validação dos campos e envio dos dados para a API
+  const formik = useFormik({
+    initialValues: {
+      cidadeOrigem: "",
+      cidadeDestino: "",
+      tipoCarro: "Básico",
+    },
+    validationSchema: Yup.object({
+      cidadeOrigem: Yup.string().required("Campo obrigatório"),
+      cidadeDestino: Yup.string().required("Campo obrigatório"),
+    }),
+    // envio dos dados para a API
+    onSubmit: async (values) => {
+      setErro(false);
 
-      console.log({ response });
+      try {
+        const response = await axios.get(
+          `/api/maps/api/distancematrix/json?origins=${values.cidadeOrigem}&destinations=${values.cidadeDestino}&units=imperial&key=AIzaSyAQlXH_lfQPVBxaO5SEifHsSVQou9d_etY`
+        );
 
-      const distanceInMeters = response.data.rows[0].elements[0].distance.value;
-      const distanceInKm = distanceInMeters / 1000;
+        const distanceInMeters =
+          response.data.rows[0].elements[0].distance.value;
+        const distanceInKm = distanceInMeters / 1000;
 
-      let valorAluguel = 0;
-      switch (tipoCarro) {
-        case "Básico":
-          valorAluguel = 1.5 * distanceInKm;
-          break;
-        case "Intermediário":
-          valorAluguel = 2.5 * distanceInKm;
-          break;
-        case "Luxo":
-          valorAluguel = 3 * distanceInKm;
-          break;
-        default:
-          break;
+        let valorAluguel = 0;
+        switch (values.tipoCarro) {
+          case "Básico":
+            valorAluguel = 1.5 * distanceInKm;
+            break;
+          case "Intermediário":
+            valorAluguel = 2.5 * distanceInKm;
+            break;
+          case "Luxo":
+            valorAluguel = 3 * distanceInKm;
+            break;
+          default:
+            break;
+        }
+
+        // envio dos dados para a página de resultado
+        router.push({
+          pathname: "/resultado",
+          query: {
+            cidadeOrigem: values.cidadeOrigem,
+            cidadeDestino: values.cidadeDestino,
+            tipoCarro: values.tipoCarro,
+            distancia: distanceInKm,
+            valorAluguel,
+          },
+        });
+      } catch (error) {
+        console.log(error);
+        setErro(true);
       }
-
-      setValorAluguel(valorAluguel);
-
-      router.push({
-        pathname: "/resultado",
-        query: {
-          cidadeOrigem,
-          cidadeDestino,
-          tipoCarro,
-          distancia: distanceInKm,
-          valorAluguel,
-        },
-      });
-    } catch (error) {
-      console.log(error);
-      setErro(true);
-    }
-  };
-
-  console.log({ cidadeOrigem });
-  console.log({ valorAluguel });
+    },
+  });
 
   return (
     <div>
       <h1>Calculadora de aluguel de carros</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Cidade de origem:</label>
+      <form className="form-wrapper" onSubmit={formik.handleSubmit}>
+        <div className="wrapper-input">
+          <label htmlFor="cidadeOrigem">Cidade de origem:</label>
           <ReactGoogleAutocomplete
+            id="cidadeOrigem"
+            required
             apiKey="AIzaSyAQlXH_lfQPVBxaO5SEifHsSVQou9d_etY"
             onPlaceSelected={(place) =>
-              setCidadeOrigem(place.formatted_address)
+              formik.setFieldValue("cidadeOrigem", place.formatted_address)
             }
           />
+          {formik.touched.cidadeOrigem && formik.errors.cidadeOrigem ? (
+            <div>{formik.errors.cidadeOrigem}</div>
+          ) : null}
         </div>
-        <div>
-          <label>Cidade de destino:</label>
+
+        <div className="wrapper-input">
+          <label htmlFor="cidadeDestino">Cidade de destino:</label>
           <ReactGoogleAutocomplete
+            id="cidadeDestino"
+            required
             apiKey="AIzaSyAQlXH_lfQPVBxaO5SEifHsSVQou9d_etY"
             onPlaceSelected={(place) =>
-              setCidadeDestino(place.formatted_address)
+              formik.setFieldValue("cidadeDestino", place.formatted_address)
             }
           />
+          {formik.touched.cidadeDestino && formik.errors.cidadeDestino ? (
+            <div>{formik.errors.cidadeDestino}</div>
+          ) : null}
         </div>
-        <div>
+
+        <div className="wrapper-input">
           <label>Tipo de carro:</label>
-          <select
-            value={tipoCarro}
-            onChange={(event) => setTipoCarro(event.target.value)}
-          >
-            <option value="Básico">Básico</option>
-            <option value="Intermediário">Intermediário</option>
-            <option value="Luxo">Luxo</option>
+          <select {...formik.getFieldProps("tipoCarro")}>
+            {tipoCarroOptions.map((option, index) => (
+              <option key={`${index} ${option.value}`} value={option.value}>
+                {option.value}
+              </option>
+            ))}
           </select>
         </div>
         <button type="submit">Calcular</button>
